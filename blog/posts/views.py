c9405c7dcebecb3791ed8612088951206c11367f
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-09-09 00:31:32 +0300

    Front page dynamic

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-
+from .models import Post
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
