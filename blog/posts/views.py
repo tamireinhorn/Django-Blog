1eba730e57dd5d1c45b7794fa78aebe38c5af23e
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-09-09 00:03:52 +0300

    Initial commit

---
+++ blog/posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
