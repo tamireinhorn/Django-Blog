c4a324c2e269ec323163bac2fe5561ea07fa0d8f
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-09-10 18:45:41 +0300

    Trying to set up Weather app

---
+++ blog/weather/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('weather', views.weather, name = 'weather')
+]
--- blog/weather/views.py
+++ blog/weather/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def weather(request):
+    return render(request, 'weather.html')
